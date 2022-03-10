8c81f998d89c9351d40fc83f41d1b8ef387997c2
wayne-shih <51947763+wayne-shih@users.noreply.github.com>
2022-03-10 20:20:21 +1300

    fix: head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import Head from '@components/Head'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head title={"WCYS & Co."} />
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
