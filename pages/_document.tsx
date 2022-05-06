f57a4b8c75ac200dd73d96bf1c452498369d8885
wayne-shih <51947763+wayne-shih@users.noreply.github.com>
2022-05-07 11:08:48 +1200

    refactor: move favicons to _documents

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+const Document = () => (
+    <Html lang="en">
+        <Head>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+    </Html>
+)
+
+export default Document;
