7172c90233b45e6fe6b3c361882b2622520a842b
wayne-shih <0x26@wayneshih.com>
2022-08-11 20:14:19 +1200

    feat: add all default tw colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const tailwind = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -8,6 +10,35 @@ module.exports = {
       sans: 'var(--font-sans)',
       serif: 'var(--font-serif)',
       mono: 'var(--font-mono)',
+    },
+    colors: {
+      inherit: 'inherit',
+      current: 'currentColor',
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      slate: tailwind.slate,
+      gray: tailwind.gray,
+      zinc: tailwind.zinc,
+      neutral: tailwind.neutral,
+      stone: tailwind.stone,
+      red: tailwind.red,
+      orange: tailwind.orange,
+      amber: tailwind.amber,
+      yellow: tailwind.yellow,
+      lime: tailwind.lime,
+      green: tailwind.green,
+      emerald: tailwind.emerald,
+      teal: tailwind.teal,
+      cyan: tailwind.cyan,
+      sky: tailwind.sky,
+      blue: tailwind.blue,
+      indigo: tailwind.indigo,
+      violet: tailwind.violet,
+      purple: tailwind.purple,
+      fuchsia: tailwind.fuchsia,
+      pink: tailwind.pink,
+      rose: tailwind.rose
     },
     extend: {
       fontFamily: {
